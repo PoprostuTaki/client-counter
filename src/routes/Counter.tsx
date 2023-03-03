cd4ef99258ea5abb54035db61410041b7ef94c00
PoprostuTaki <rafpiekar@gmail.com>
2023-03-03 21:06:29 +0100

    Added limit to saved history and set initial value of client to last value from current day.

--- src/routes/Counter.tsx
+++ src/routes/Counter.tsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
 
+interface example {
+  [key: string]: number;
+}
+
 function Counter() {
-  const [clientNumber, setClientNumber] = useState(0);
+  const [clientNumber, setClientNumber] = useState(() => {
+    const localStorageData = localStorage.getItem("client-counter");
+    if (localStorageData !== null) {
+      const actualDate = new Date().toLocaleDateString();
+      const parsedData: example[] = JSON.parse(localStorageData);
+      const getFirstElementFromParsedData: example = parsedData[0];
+      const keyValuePair = Object.entries(getFirstElementFromParsedData);
+      const dateFromLocalStorage = keyValuePair[0][0];
+      const valueFromLocalStorage = keyValuePair[0][1];
+      if (dateFromLocalStorage === actualDate) return valueFromLocalStorage;
+    }
+    return 0;
+  });
 
   const addOneClient = () => {
     setClientNumber((currentValue) => {
@@ -21,6 +37,27 @@ function Counter() {
   const resetCounter = () => {
     setClientNumber(0);
   };
+  const date = new Date().toLocaleDateString();
+
+  const saveCounter = () => {
+    const localStorageData = localStorage.getItem("client-counter");
+    if (localStorageData === null) {
+      localStorage.setItem(
+        "client-counter",
+        JSON.stringify([{ [date]: clientNumber }])
+      );
+    } else {
+      let parsedlocalStorageData = JSON.parse(localStorageData);
+      if (parsedlocalStorageData.length > 50) {
+        parsedlocalStorageData.pop();
+      }
+
+      localStorage.setItem(
+        "client-counter",
+        JSON.stringify([{ [date]: clientNumber }, ...parsedlocalStorageData])
+      );
+    }
+  };
 
   return (
     <div className="container flex max-w-2xl flex-1 flex-col justify-center gap-4 p-4">
@@ -30,13 +67,13 @@ function Counter() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="h-6 w-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
             />
           </svg>
@@ -46,6 +83,7 @@ function Counter() {
 
       <div className="flex items-center justify-between gap-4">
         <button
+          onClick={saveCounter}
           title="Save"
           className="flex h-16 w-1/3 items-center justify-center rounded-3xl border-2 border-slate-700 p-2 transition duration-300 hover:bg-zinc-900 hover:text-white"
           type="button"
@@ -54,13 +92,13 @@ function Counter() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="h-6 w-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M9 13.5l3 3m0 0l3-3m-3 3v-6m1.06-4.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
             />
           </svg>
@@ -85,13 +123,13 @@ function Counter() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="h-6 w-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
             />
           </svg>
@@ -108,13 +146,13 @@ function Counter() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="h-6 w-6"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
           />
         </svg>
