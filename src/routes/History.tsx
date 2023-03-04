d35d12f879728d7d83ca64b465ecd0bb0cdd9539
PoprostuTaki <rafpiekar@gmail.com>
2023-03-04 23:35:09 +0100

    Added History page

--- src/routes/History.tsx
+++ src/routes/History.tsx
@@ -1,3 +1,37 @@
+import { useState } from "react";
+interface ClientsInDate {
+  [key: string]: number;
+}
 export function History() {
-  return <div className="">History</div>;
+  const [clientsList, setClientsList] = useState(() => {
+    const dataFromLocalStorage = localStorage.getItem("client-counter");
+    if (dataFromLocalStorage !== null) {
+      return JSON.parse(dataFromLocalStorage);
+    }
+    return undefined;
+  });
+
+  return (
+    <ul className="container flex max-w-xl flex-1 flex-col justify-center divide-y p-4 ">
+      <li className="flex h-12 items-center justify-between px-4">
+        <span>Date</span>
+        <span>Clients</span>
+      </li>
+      {clientsList ? (
+        clientsList.map((item: ClientsInDate, index: number) => {
+          return (
+            <li
+              key={`${Object.keys(item)[0]} &{index}`}
+              className="flex h-12 items-center justify-between px-4"
+            >
+              <span>{Object.keys(item)[0]}</span>
+              <span>{Object.values(item)[0]}</span>
+            </li>
+          );
+        })
+      ) : (
+        <li>asd</li>
+      )}
+    </ul>
+  );
 }
